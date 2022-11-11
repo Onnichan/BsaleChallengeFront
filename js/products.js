5a4a5dbc227fa375eb6b0b2758e3d6827662500f
Onnichan <dyangeltk17@gmail.com>
2022-11-11 16:35:23 -0500

    :sparkles: Add card by product

--- js/products.js
+++ js/products.js
@@ -1,28 +1,25 @@
 const grid = document.getElementById("grid");
 
-
-// window.onload(async() => {
 (async () => {
   const resp = await getAllProducts();
   console.log(resp);
+  renderGridCard(resp);
 })();
 
 
 function renderGridCard(data){
-  const gridcard="";
+  let gridcard="";
 
   for (let i = 0; i < data.rows.length; i++) {
-    const element = data.row[i];
+    let product = data.rows[i];
+    gridcard += `<div class="card" style="width: 18rem;">
+      <img src="${product.url_image}" class="card-img-top" alt="...">
+      <div class="card-body">
+        <span class="text-bold">${product.name}</span>
+        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        <span>Price: $${product.price}</span>
+      </div>
+    </div>`;
   }
-  
-  gridcard += '<div class="card" style="width: 18rem;">';
-  gridcard +=  '<img src="..." class="card-img-top" alt="...">'
-  //   <div class="card-body">
-  //     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-  //   </div>
-  // </div>'
-
   grid.innerHTML = gridcard;
 }
-  // console.log(resp);
-// })
