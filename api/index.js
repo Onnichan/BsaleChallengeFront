d792c2c7bdb9c8fc1f587cfa618c75e68b3e59fd
Onnichan <dyangeltk17@gmail.com>
2022-11-12 19:57:15 -0500

    :sparkles: Add pagination, filters and categories

--- api/index.js
+++ api/index.js
@@ -1,13 +1,30 @@
 const API_URL = "http://localhost:4000/api/v1";
 
-async function getAllProducts(){
-  const resp = await fetch(`${API_URL}/products`);
+async function getAllProducts(offset = 5, limit = 5) {
+  const resp = await fetch(
+    `${API_URL}/products?offset=${offset}&limit=${limit}`
+  );
   const products = await resp.json();
   return products;
 }
 
-async function getFilteredProducts(){
-  const resp = await fetch(API_URL);
+async function getAllCategories() {
+  const resp = await fetch(`${API_URL}/categories`);
+  const categories = await resp.json();
+  return categories;                                                                                                                                                                  
+}
+
+async function searchProduct(search) {
+  console.log(search);
+  const resp = await fetch(`${API_URL}/products/${search}`);
+  const filteredProducts = await resp.json();
+  return filteredProducts;
+}
+
+async function getProductsByCategory(offset = 5, limit = 5, category) {
+  const resp = await fetch(
+    `${API_URL}/categories/${category}?offset=${offset}&limit=${limit}`
+  );
   const products = await resp.json();
   return products;
 }
--- js/products.js
+++ js/products.js
@@ -1,25 +1,104 @@
 const grid = document.getElementById("grid");
+const pagination = document.getElementById("pagination");
+let cantProductByPage;
+let cantProducts;
 
 (async () => {
-  const resp = await getAllProducts();
-  console.log(resp);
-  renderGridCard(resp);
+  // const products = await showProducts();
+  // cantProducts = products.count;
+  // renderGridCard(products);
+  // renderPagination(products);
+  renderHTML();
 })();
 
+/**
+ *
+ * @param {*} index For the offset field
+ */
+async function renderHTML(index = 1) {
+  const products = await showProducts(index);
+  cantProducts = products.count;
+  renderGridCard(products.rows);
+  renderPagination(cantProducts);
+  renderCategories();
+}
 
-function renderGridCard(data){
-  let gridcard="";
+function renderGridCard(data) {
+  let gridcard = "";
 
-  for (let i = 0; i < data.rows.length; i++) {
-    let product = data.rows[i];
+  for (let i = 0; i < data.length; i++) {
+    let product = data[i];
     gridcard += `<div class="card" style="width: 18rem;">
       <img src="${product.url_image}" class="card-img-top" alt="...">
       <div class="card-body">
-        <span class="text-bold">${product.name}</span>
-        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-        <span>Price: $${product.price}</span>
+        <span class="text-bold">${product.name}</span><br>
+        <span>Price: $${product.price}</span><br><br>
+        <button class="btn btn-outline btn-primary">Add to cart</button>
       </div>
     </div>`;
   }
   grid.innerHTML = gridcard;
+}
+
+function renderPagination(cantProducts) {
+  let paginate = "";
+  let productByPage = Math.ceil(cantProducts / cantProductByPage);
+  paginate += `<li class="page-item">
+    <a class="page-link" href="#">Previous</a>
+  </li>`;
+  for (let i = 0; i < productByPage; i++) {
+    paginate += `<li class="page-item"><a class="page-link" href="#" onclick="renderHTML(${i})">${i+1}</a></li>`;
+  }
+  paginate += `<li class="page-item"><a class="page-link" href="#">Next</a></li>`;
+  pagination.innerHTML = paginate;
+}
+
+async function showProducts(index) {
+  cantProductByPage = document.getElementById("productsByPage").value;
+  const resp = await getAllProducts(
+    cantProductByPage * index,
+    cantProductByPage
+  );
+  return resp;
+}
+
+async function renderProduct(element, e) {
+  // console.log(e);
+  if (e.keyCode == 13) {
+    // console.log(element.value);
+    const findProduct = await searchProduct(element.value);
+    renderGridCard(findProduct.rows);
+    renderPagination(findProduct.count);
+    console.log(findProduct);
+  }
+}
+
+async function renderCategories() {
+  const asideCategories = document.getElementById("aside-categories");
+  let htmlCategories = `<h3 class="text-muted text-center">Categories</h3>`;
+  htmlCategories += `<nav class="d-flex flex-column gap-2 p-5">`;
+
+  const categories = await getAllCategories();
+
+  categories.forEach(category => {
+    console.log(category.name);
+    htmlCategories += `<button type="button" onclick="renderProductsByCategory('${category.name}')" class="btn btn-dark">${category.name}</button>`;
+  });
+
+  htmlCategories += `</nav>`;
+  asideCategories.innerHTML = htmlCategories;
+}
+
+async function renderProductsByCategory(category, index = 1) {
+  console.log(category);
+  cantProductByPage = document.getElementById("productsByPage").value;
+  const productsByCategory = await getProductsByCategory(
+    cantProductByPage*0,
+    cantProductByPage,
+    category
+  );
+
+  console.log(productsByCategory);
+  renderGridCard(productsByCategory.rows[0].products);
+  renderPagination(productsByCategory.count);
 }
